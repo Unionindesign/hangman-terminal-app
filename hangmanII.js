9e1959a1bac19cbee0579f59da2b6410f58e2004
iMac <imac@SM-iMac.local>
2018-02-04 22:14:19 -0700

    a few bugs in letters..

--- hangmanII.js
+++ hangmanII.js
@@ -1,22 +1,40 @@
 const inquirer = require('inquirer');
 const Word = require('./words.js');
+const Letter = require('./letters.js');
 
 var wordsArray = ['node', 'javascript', 'react', 'fullstack', 'mongodb', 'mysql', 'angular', 'MERN', 'css', 'java', 'html',];  // '',
-
 var currentWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
-function startGame () {
+var	guessCount = 12;
+var letterInSelectedWord = [];
+// var numberBlanks = 0;
+// var blankOrLetters = [];
+
+function startGame() {
     var wordPicked = new Word(currentWord);
     wordPicked.printBlanksArray();
-    wordPicked.printCurrentWord();
-    
+    // wordPicked.printCurrentWord();   Don't show them the answer! but works
+}
+
+function letterChecker() {
+    // var userGuess = process.argv[2];
+    var userGuess = new Letter();
+    inquirer.prompt(
+        [
+            {   
+                name: 'userGuess',
+                type: 'input',
+                message: 'Guess a Letter:'
+            }
+        ]).then(answers => {
+            var userGuess = new Letter(answers);
+            userGuess.printLetterOrBlank();
+        });
 }
 
 startGame();
+letterChecker();
 
-function letterChecker () {
-    
-}
 
 // inquirer - ask for a guess
 // go through the wordPicked, compare userGuess (stored in a variable), iterate thru word array with a loop to compare 
